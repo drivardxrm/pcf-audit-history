48da6e330721c11d18245482c798ccdd0ab2a5d8
Mário Rocha <mario@novalogica.pt>
2025-01-24 11:37:30 +0000

    Added no results translations

--- AuditHistory/components/history.tsx
+++ AuditHistory/components/history.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { Audit } from '../interfaces/audit';
 import { AuditCard } from './cards/audit';
 import { Subtitle2 } from '@fluentui/react-components';
+import { useContext } from 'react';
+import { ControlContext } from '../context/control-context';
 
 interface IHistoryProps {
     audits?: Audit[]
 }
 
 export default function History({ audits }: IHistoryProps) {
+    const { resources } = useContext(ControlContext);
+    
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 2, gap: 8 }}>
             {
@@ -23,7 +27,7 @@ export default function History({ audits }: IHistoryProps) {
                             height: '200px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'
                         }}
                     >
-                        <Subtitle2>No audit history found</Subtitle2>
+                        <Subtitle2>{resources.getString("no-results")}</Subtitle2>
                     </div>
                 )
             }
